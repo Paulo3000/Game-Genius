c09544a2f159d6083cc3e40c6fd6a98e219ce09a
Paulo3000 <1988paulosergio@gmail.com>
2021-03-25 09:25:22 -0300

    first commit

---
+++ Genius2/Apoio.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading;
+
+namespace Genius2
+{
+    class Apoio
+    {
+       // public string mesagem1 = "Digite os números da sequência";
+        private int duracao = 300;
+        public char[] coleta = new char[4];
+        public int velocidade = 600;
+
+        public int Laco(int num2)
+        {
+
+            
+
+            Random random = new Random();
+            char[] letras = new char[] { 'Y', 'G', 'B', 'R' };
+
+            for (int i = 0; i <letras.Length; i++)
+            {
+                int numero = random.Next(0, 3);
+                Console.Write(letras[numero] + " ");
+                coleta[i] = letras[numero];
+                switch (letras[numero])
+                {
+                    case 'Y':
+                        Console.Beep(700, duracao);
+                        break;
+                    case 'G':
+                        Console.Beep(900, duracao);
+                        break;
+                    case 'B':
+                        Console.Beep(600, duracao);
+                        break;
+                    case 'R':
+                        Console.Beep(400, duracao);
+                        break;
+
+                }
+                Thread.Sleep(velocidade);
+            }
+            Console.WriteLine("\n\n");
+            return num2;
+            // configração do beep  Console.Beep(tom,duração);
+        }
+
+
+        public char Som(char som)
+        {
+            switch (som)
+            {
+                case 'Y':
+                    Console.Beep(700, duracao);
+                    break;
+                case 'G':
+                    Console.Beep(900, duracao);
+                    break;
+                case 'B':
+                    Console.Beep(600, duracao);
+                    break;
+                case 'R':
+                    Console.Beep(400, duracao);
+                    break;
+                default:
+                    Console.Beep(400, 1900);
+                    break;
+            }
+            return som;
+            
+        }
+
+
+
+
+
+    }
+}
---
+++ Genius2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading;
+
+namespace Genius2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+
+
+          //  int faze = 0;
+
+            Apoio apoio = new Apoio();
+
+        volta:
+            Console.WriteLine("entre com 0 pra começar a sequência");
+            int num = int.Parse(Console.ReadLine());
+            if (num != 0)
+            {
+                Console.Clear();
+                Console.Beep();
+                goto volta;
+
+            }
+
+
+      
+
+            Console.WriteLine("iniciando sequencia\n");
+
+            while (true)
+            {
+               // faze++;
+                apoio.Laco(num);
+                Console.Clear();
+                Console.WriteLine("Entre com a sequencia das letras\n");
+
+
+                char[] numeroEscolha = new char[4];
+                for (int i = 0; i <= 3; i++)
+                {
+
+                    numeroEscolha[i] = Console.ReadKey().KeyChar;
+                    Console.WriteLine("\n");
+                    apoio.Som(numeroEscolha[i]);
+                    if (numeroEscolha[0] == apoio.coleta[0] && i == 0)
+                    {
+
+                        Console.WriteLine("---------------OK");
+
+                    }
+                    else if (numeroEscolha[1] == apoio.coleta[1] && i == 1)
+                    {
+                        Console.WriteLine("---------------OK");
+
+                    }
+                    else if (numeroEscolha[2] == apoio.coleta[2] && i == 2)
+                    {
+                        Console.WriteLine("---------------OK");
+
+
+                    }
+                    else if (numeroEscolha[3] == apoio.coleta[3] && i == 3)
+                    {
+                        Console.WriteLine("---------------OK");
+
+                    }
+
+
+
+                    else
+                    {
+                        Console.WriteLine("------------ERRO");
+                        Console.Beep(400, 1900);
+                        break;
+                    }
+
+                }
+
+            }
+        }
+    }
+}
